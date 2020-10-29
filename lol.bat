d98a3eddacb63135ad5db9956b574554bcaac82b
Irian3x3 <irian3x3@gmail.com>
2020-10-29 11:53:43 -0400

    upload the fake virus file

---
+++ lol.bat
@@ -0,0 +1,24 @@
+@echo off
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+msg * Hey young fella. Looks like you have been hacked!
+msg * Hi again!
+msg * Click OK to remove the hack
+msg * No I was joking it won't remove it
+msg * So how are you?
